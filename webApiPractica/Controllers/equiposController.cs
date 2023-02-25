78e29fe42c9a24446ed8d8084b75bad96a9d7d1e
rvvil <rvvil@DESKTOP-POIK77V>
2023-02-25 10:25:40 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+        public equiposController(equiposContext equiposContext) 
+        {
+            _equiposContext= equiposContext;
+
+
+        }
+
+    }
+}
